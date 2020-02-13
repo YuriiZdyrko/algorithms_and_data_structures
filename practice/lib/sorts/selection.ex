7bbd0826e1ceb4c6e408227fa76134bd53942a08
banzay <yuriy.zdyrko@gmail.com>
2020-02-13 20:12:15 +0200

    selection sort

---
+++ practice/lib/sorts/selection.ex
@@ -0,0 +1,24 @@
+defmodule Sorts.Selection do
+  @moduledoc """
+  On each iteration find smallest item in right (unsorted) part
+  """
+
+  def run do
+    list = [3, 5, 10, 1, 55, 20, 100, 2]
+    sort(list)
+  end
+
+  def sort([]), do: []
+  def sort([h | t] = list) do
+    {min, rest} = min_and_rest(t, h)
+    [min | sort(rest)]
+  end
+  
+  def min_and_rest(list, min, rest \\ [])
+  def min_and_rest([], min, rest), do: {min, rest}
+  def min_and_rest([h | t] = list, min, rest) do
+    if h < min,
+        do: min_and_rest(t, h, [min | rest]),
+        else: min_and_rest(t, min, [h | rest])
+  end
+end
