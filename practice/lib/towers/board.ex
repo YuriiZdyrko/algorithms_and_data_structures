247f918aeedf6a46cf87b7d8fa7c45034a1820dd
banzay <yuriy.zdyrko@gmail.com>
2020-02-22 18:55:30 +0200

    towers - 3

--- practice/lib/towers/board.ex
+++ practice/lib/towers/board.ex
@@ -1,11 +1,14 @@
 defmodule Towers.Board do
   defstruct cells: []
 
+  import IEx
   alias Towers.{Board, Row, Cell}
 
   @board_size 4
 
-  def new(clues) do
+  @clues [2, 2, 1, 3, 2, 2, 3, 1, 1, 2, 2, 3, 3, 2, 1, 3]
+
+  def new(clues \\ @clues) do
     cells =
       for x <- 0..(@board_size - 1), y <- 0..(@board_size - 1) do
         Cell.new(y, x)
@@ -28,17 +31,65 @@ defmodule Towers.Board do
     Enum.count(cells, &Cell.discovered?(&1))
   end
 
-  def merge_row(board, %Row{cells: []} = _row), do: board
+  def split_rows(board) do
+    [
+      front_vert,
+      back_hor,
+      back_vert,
+      front_hor
+    ] = Enum.chunk_every(@clues, @board_size)
 
-  def merge_row(board, %Row{cells: [h | t]} = _row) do
-    updated_cell = %Cell{x: x, y: y} = h
+    hor_rows_cells = board.cells
 
-    List.update_at(
-      board.cells,
-      y,
-      fn row ->
-        List.replace_at(row, x, updated_cell)
-      end
-    )
+    vert_rows_cells =
+      board.cells
+      |> Enum.zip()
+      |> Enum.map(&Tuple.to_list(&1))
+
+    hor_rows = init_rows(hor_rows_cells, Enum.reverse(front_hor), back_hor)
+    vert_rows = init_rows(vert_rows_cells, front_vert, Enum.reverse(back_vert))
+
+    hor_rows ++ vert_rows
+  end
+
+  def init_rows(cells, front_clues, back_clues) do
+    cells
+    |> Enum.zip(front_clues)
+    |> Enum.zip(back_clues)
+    |> Enum.map(fn {{row_cells, front_clue}, back_clue} ->
+      Row.new(front_clue, back_clue, row_cells)
+    end)
+  end
+
+  def merge_row(board, %Row{cells: cells}),
+    do: merge_row(board, cells)
+
+  def merge_row(board, []), do: board
+
+  def merge_row(board, [h | t]) do
+    replace_cell(board, h)
+    |> merge_row(t)
+  end
+
+  def replace_cell(board = %Board{cells: cells}, cell) do
+    cells =
+      List.update_at(
+        cells,
+        cell.y,
+        fn row ->
+          List.replace_at(row, cell.x, cell)
+        end
+      )
+
+    %Board{
+      board
+      | cells: cells
+    }
+  end
+
+  def cell_at(board, x, y) do
+    board.cells
+    |> Enum.at(y)
+    |> Enum.at(x)
   end
 end
--- practice/test/towers/board_test.exs
+++ practice/test/towers/board_test.exs
@@ -1,6 +1,6 @@
 defmodule Towers.BoardTest do
   use ExUnit.Case
-  alias Towers.{Board, Cell}
+  alias Towers.{Board, Cell, Row}
 
   test "new board generates 4X4 cells grid" do
     clues = []
@@ -15,18 +15,44 @@ defmodule Towers.BoardTest do
            } = Board.new(clues)
   end
 
+  test "cell_at" do
+    board = Board.new()
+    assert %Cell{x: 1, y: 2} == Board.cell_at(board, 1, 2)
+  end
+
   test "merge row" do
-    # %Board{
-    #     cells: [
-    #         %Cell{
-    #             x: 0,
-    #             y: 0
-    #         },
-    #         %Cell{
-    #             x: 0,
-    #             y: 0
-    #         }
-    #     ]
-    # }
+    map_set_1 = MapSet.new()
+    map_set_2 = MapSet.new([2, 3])
+
+    board =
+      Board.new()
+      |> Board.merge_row(%Row{
+        cells: [
+          %Cell{x: 1, y: 2, value: 1, values: MapSet.new()},
+          %Cell{x: 1, y: 3, value: nil, values: MapSet.new([2, 3])}
+        ]
+      })
+
+    %Cell{value: 1, values: ^map_set_1} = Board.cell_at(board, 1, 2)
+    %Cell{value: nil, values: ^map_set_2} = Board.cell_at(board, 1, 3)
+  end
+
+  test "split rows" do
+    assert [
+             hor_1,
+             hor_2,
+             %Row{},
+             %Row{},
+             ver_1,
+             ver_2,
+             %Row{},
+             %Row{}
+           ] = Board.new() |> Board.split_rows()
+
+    assert %Row{n_front: 3, n_back: 2} = hor_1
+    assert %Row{n_front: 1, n_back: 2} = hor_2
+
+    assert %Row{n_front: 2, n_back: 3} = ver_1
+    assert %Row{n_front: 2, n_back: 2} = ver_2
   end
 end
